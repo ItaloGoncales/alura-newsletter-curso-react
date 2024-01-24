d77a98bcdc8fcc53404fa5377fd9850012d590d2
Italo Gonçales <italo.goncales@gmail.com>
2024-01-24 08:00:53 -0300

    [Curso React - Aula 05] Movendo classes e instalando plugins

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -20,10 +20,7 @@ const Form = ({ onSubmit }) => {
       <Input type="text" required placeholder="Insira seu nome..." />
       <Input type="email" required placeholder="Insira seu email..." />
 
-      <button
-        type="submit"
-        className={`bg-alura-100 dark:bg-dark-200 w-full max-w-sm rounded-full text-gray-200 py-1 px-5 uppercase text hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black`}
-      >
+      <button type="submit" className="alura-button">
         Seguir
       </button>
     </form>
